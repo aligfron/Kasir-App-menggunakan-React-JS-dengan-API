2c3fe7cff0fd3566cc7a9457c60ab773f749282c
aligfron <vrondy.twib@gmail.com>
2024-11-19 14:07:01 +0700

    - install react js - install react bootstarp - install sweet alert - install axios

---
+++ kasirapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarComponents from "./components/NavbarComponents";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponents />
+    </div>
+  );
+}
+
+export default App;
